87fb86c84c6e633309bf022ea15d0ca65212c54a
System Administrator <root@Pavels-MacBook-Pro.local>
2022-09-23 23:57:03 +0300

    Removed Main.storyboard and SceneDelegate related code and configs

--- GithubBrowser/RootViewController.swift
+++ GithubBrowser/RootViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  GithubBrowser
 //
-//  Created by Pavel Palancica on 9/21/22.
+//  Created by Pavel Palancica on 9/23/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
